1a0ea3216dc2115753a04324d777de10f97c997a
S0AndS0 <strangerthanbland@gmail.com>
2019-10-23 00:19:00 -0700

    :bug: Corrects for promises usage

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const get_gha_input = function(name) { return process.env[`INPUT_${name.toUpperC
 
 
 const actor = process.env.GITHUB_ACTOR;
-const repository = process.env.GITHUB_REPOSITORY
+const repo = process.env.GITHUB_REPOSITORY.split('/')[1];
 
 
-if (actor === undefined || repository === undefined) {
+if (actor === undefined || repo === undefined) {
   const error_message = [
     'Environment variable `GITHUB_ACTOR` or `GITHUB_REPOSITORY` is undefined',
     'Please ensure that you are testing with something like...',
@@ -20,6 +20,7 @@ if (actor === undefined || repository === undefined) {
     '',
     '... hint if errors about undefined Inputs then pop, try...',
     '  GITHUB_ACTOR=your-name\\',
+    '  GITHUB_REPOSITORY=fancy-project\\',
     '  INPUT_PULL_REQUEST_TOKEN=...\\',
     '  node index.js',
   ];
@@ -118,34 +119,29 @@ const title = get_gha_input('title');
 const body = get_gha_input('body');
 
 
-let response;
-try {
-  response = octokit.pulls.create({
-    'title': title,                 // Commit title, generally should be less than 74 characters
-    'body': body,                   // Multi-line commit message
-    'owner': actor,                 // Username or Organization with permissions to initialize Pull Request
-    'repo': repository,             // GitHub repository link or hash eg. `org-or-name/repository`
-    'head': head,                   // Where changes are implemented, eg. `your-name:feature-branch`
-    'base': base,                   // Branch name where changes should be incorporated, eg. `master`
-    'maintainer_can_modify': true,  // Not about to assume that maintainers do not want the option to modify
-    'draft': false,                 // If `true` no notifications would be generated
-  });
-} catch (e) {
-  const error_message = ['Error initializing Pull Request',
-                         ...error_message__base,
-                         ...gha_example,
-  ];
-
-  throw new Error(error_message.join('\n'));
-}
-
-if (response) {
-  console.table(response);
-} else {
+const response = octokit.pulls.create({
+  'title': title,                 // Commit title, generally should be less than 74 characters
+  'body': body,                   // Multi-line commit message
+  'owner': actor,                 // Username or Organization with permissions to initialize Pull Request
+  'repo': repo,                   // GitHub repository link or hash eg. `fancy-project`
+  'head': head,                   // Where changes are implemented, eg. `your-name:feature-branch`
+  'base': base,                   // Branch name where changes should be incorporated, eg. `master`
+  'maintainer_can_modify': true,  // Not about to assume that maintainers do not want the option to modify
+  'draft': false,                 // If `true` no notifications would be generated
+}).catch((e) => {
   const error_message = ['Failed to initialize Pull Request',
                          ...error_message__base,
                          ...gha_example,
   ];
-
-  throw new Error(error_message.join('\n'));
-}
+  console.error(error_message.join('\n'));
+  throw e;
+}).then(function(r) {
+  // console.log(JSON.stringify(r['headers']));
+  // console.log(JSON.stringify(r['data']));
+  console.log(`Rate Limit Remaining -> ${r['headers']['x-ratelimit-remaining']}`);
+  console.log(`Rate Limit Reset -> ${r['headers']['x-ratelimit-reset']}`);
+  console.log(`Pull Request HTML URL -> ${r['data']['html_url']}`);
+  console.log(`Pull Request Number -> ${r['data']['number']}`);
+  console.log(`Pull Request State -> ${r['data']['state']}`);
+  return r;
+});
